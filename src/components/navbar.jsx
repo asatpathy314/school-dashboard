4433181faf7d078f78df72756bcd00f829c2e1ea
Rayna Bhattacharyya <raynabhattacharyya@gmail.com>
2024-05-20 13:15:24 -0700

    added navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/navbar.css';
+
+const Navbar = () => {
+  return (
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="dashboard">Dashboard</a></li>
+        <li><a href="studentTeacherDir">Directory</a></li>
+        <li><a href="calendar">Calendar</a></li>
+        
+    </ul>
+  )
+}
+
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import './styles/index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import StudentTeacherDir from './roots/StudenTeacherDir.jsx'
+import Home from './roots/Home.jsx'
+import Class from './roots/Class.jsx'
+import Dashboard from './roots/Dashboard.jsx'
+import Calendar from './roots/Calendar.jsx'
+import Navbar from './components/navbar.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "/class",
+    // when firebase is set up loop through all possible classes and add route to that class using children (I added an example id)
+    children: [
+      {
+        path: "wejifhwejh",
+        elem: <Class id="wejifhwejh"></Class>
+      }
+    ],
+    element: <Home/>
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard/>
+  },
+  {
+    path: "/studentTeacherDir",
+    element: <StudentTeacherDir/>
+  },
+  {
+    path: "/calendar",
+    element: <Calendar/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <Navbar/>
+    <RouterProvider router={router}/>
+  </>
 )
