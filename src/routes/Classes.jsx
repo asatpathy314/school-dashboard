e90ccc51505a3dea55c63a8ded7bb1be9254222a
Patrick Yuson <ntu7de@virginia.edu>
2024-05-21 17:41:10 -0400

    Created a working version of Classes by adding dummy data

--- src/routes/Classes.jsx
+++ src/routes/Classes.jsx
@@ -3,10 +3,19 @@ import Dir from '../components/Dir';
 import Map from '../components/Map'
 
 const Classes = () => {
+
+  const data = [
+    { fullName: 'John Doe', id: '123456' },
+    { fullName: 'Jane Smith', id: '234567' },
+    { fullName: 'Michael Johnson', id: '345678' },
+    { fullName: 'Emily Brown', id: '456789' },
+    { fullName: 'William Taylor', id: '567890' },
+  ];
+
   return (
     <div>
       {/* Replace div with component */}
-      <Dir type="Class" comp={<Map ids={true} personNames={true} />}></Dir>
+      <Dir type="Class" comp={<Map ids={true} personNames={true} data={data}/>}></Dir>
     </div>
   )
 }
