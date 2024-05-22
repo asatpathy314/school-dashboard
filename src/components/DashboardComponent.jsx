f2251c67944968a6cb80e9e50abc0cf4a94e5cc5
Patrick Yuson <ntu7de@virginia.edu>
2024-05-22 00:31:13 -0400

    Changed if statements to a switch statement in the DashboardComponent file

--- src/components/DashboardComponent.jsx
+++ src/components/DashboardComponent.jsx
@@ -1,50 +1,43 @@
-import React from 'react'
-import '../styles/dashboard/Dashboard.css'
-import Map from './Map'
+import React from 'react';
+import '../styles/dashboard/Dashboard.css';
+import Map from './Map';
 
 const DashboardComponent = ({ data, which }) => {
+  let content;
 
+  switch (which) {
+    case 'teacher':
+      content = (
+        <div className='dashboard-teachers'>
+          <h3 className='h3'>Teachers</h3>
+          <input placeholder="Search by name..." />
+          <Map ids={true} personNames={true} data={data} />
+        </div>
+      );
+      break;
+    case 'student':
+      content = (
+        <div className='dashboard-students'>
+          <h3 className='h3'>Students</h3>
+        </div>
+      );
+      break;
+    case 'class':
+      content = (
+        <div className='dashboard-classes'>
+          <h3 className='h3'>Classes</h3>
+        </div>
+      );
+      break;
+    default:
+      content = (
+        <div className='dashboard-upcoming-events'>
+          <h3 className='h3'>Upcoming Events</h3>
+        </div>
+      );
+  }
 
-    if (which == 'teacher') {
-        return (
-            <>
-                <div className='dashboard-teachers'>
-                    <h3 className='h3'>Teachers</h3>
-                    <input placeholder="Search by name..."></input>
-                    <Map ids={true} personNames={true} data={data}/>
-                </div>
-            </>
-          )
-    }
+  return <>{content}</>;
+};
 
-    if (which == 'student') {
-        return (
-            <>
-                <div className='dashboard-students'>
-                    <h3 className='h3'>Students</h3>
-                </div>
-            </>
-        )
-    }
-
-    if (which == 'class') {
-        return (
-            <>
-                <div className='dashboard-classes'>
-                    <h3 className='h3'>Classes</h3>
-                </div>
-            </>
-        )
-    }
-
-    return (
-        <>
-            <div className='dashboard-upcoming-events'>
-                <h3 className='h3'>Upcoming Events</h3>
-            </div>
-        </>
-    )
-
-}
-
-export default DashboardComponent
+export default DashboardComponent;
