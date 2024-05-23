26cf727fb60a9ecc1afe0a39a8624173cb55398a
Patrick Yuson <ntu7de@virginia.edu>
2024-05-23 15:26:03 -0400

    Resolved StudentDir problem... (?)

--- src/routes/StudentDir.jsx
+++ src/routes/StudentDir.jsx
@@ -10,7 +10,6 @@ const StudentDir = () => {
     async function getStudents() {
       const collRef = collection(db, "students");
       const studentSnapshot = await getDocs(query(collRef));
-      // console.log(studentSnapshot)
       let temp = [];
       
       await Promise.all(studentSnapshot.docs.map(async (doc) => {
@@ -20,7 +19,6 @@ const StudentDir = () => {
 
           let grades = 0;
           let count = 0;
-          // console.log(classes)
           const gradeSum = classes.forEach(classItem => {
             grades = classItem.grade + grades;
             count = count + 1;
