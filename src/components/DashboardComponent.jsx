29f4c3f4ced164125ce8892129de89a0247747f9
Patrick Yuson <ntu7de@virginia.edu>
2024-05-22 23:33:27 -0400

    Reached stopping point for Dashboard styling
    will work on populating dashboard with data next

--- src/components/DashboardComponent.jsx
+++ src/components/DashboardComponent.jsx
@@ -32,36 +32,53 @@ const DashboardComponent = ({ data, which }) => {
     default:
       content = (
         <div className='dashboard-upcoming-events'>
-          <h3>Upcoming Events</h3>
-          <Box sx={{ maxHeight: '20vh', 
-            overflowY: 'auto',
-          }}>
-              {data.map(event => (
-                <Card key={event.id} sx={{ 
-                  display: 'flex', 
-                  mr: 4, 
-                  ml: 4,
-                  mb: 3,
-                  boxShadow: 8,
-                  width: '40vw',
-                }}>
-                  <Box sx={{ 
-                    display: 'flex', 
-                    flexDirection: 'column', 
-                    justifyContent: 'center', 
-                    alignItems: 'center', 
-                    padding: 1,
-                  }}>
-                    <Typography variant="body1">{event.startDate.toDateString()}</Typography>
-                    <Typography variant="body2" color="text.secondary" >{event.startHour}</Typography>
-                  </Box>
-                  <CardContent>
-                    <Typography variant="h6">{event.name}</Typography>
-                  </CardContent>
-                </Card>
-              ))}  
-          </Box> 
+          <h3 className='h3'>Upcoming Events</h3>
+          <Box className="card-container">
+            {data.map(event => (
+              <Card key={event.id} className="event-card">
+                <Box className="event-date">
+                  <Typography variant="body1">{event.startDate.toDateString()}</Typography>
+                  <Typography variant="body2" color="text.secondary">{event.startHour}</Typography>
+                </Box>
+                <CardContent>
+                  <Typography variant="h6">{event.name}</Typography>
+                </CardContent>
+              </Card>
+            ))}  
+          </Box>
         </div>
+        // <div className='dashboard-upcoming-events'>
+        //   <h3 className='h3'>Upcoming Events</h3>
+        //   <Box sx={{ maxHeight: '20vh', 
+        //     overflowY: 'auto',
+        //   }}>
+        //       {data.map(event => (
+        //         <Card key={event.id} sx={{ 
+        //           display: 'flex', 
+        //           mr: 4, 
+        //           ml: 6,
+        //           mb: 3,
+        //           mt: 1,
+        //           boxShadow: 8,
+        //           width: '40vw',
+        //         }}>
+        //           <Box sx={{ 
+        //             display: 'flex', 
+        //             flexDirection: 'column', 
+        //             justifyContent: 'center', 
+        //             // alignItems: 'center', 
+        //             padding: 2,
+        //           }}>
+        //             <Typography variant="body1">{event.startDate.toDateString()}</Typography>
+        //             <Typography variant="body2" color="text.secondary" >{event.startHour}</Typography>
+        //           </Box>
+        //           <CardContent>
+        //             <Typography variant="h6">{event.name}</Typography>
+        //           </CardContent>
+        //         </Card>
+        //       ))}  
+        //   </Box> 
+        // </div>
       );
   }
 
