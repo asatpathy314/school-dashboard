4a2b46b07794ce60b3729f159bbd30c27dd45ad2
Patrick Yuson <ntu7de@virginia.edu>
2024-05-24 08:51:37 -0400

    Set dashboard data grids to have a fixed size of 10 entries

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -242,10 +242,10 @@ const Map = (props) => {
                     initialState={{
                         ...data.initialState,
                         pagination: {
-                            paginationModel: { page: 0, pageSize: 5 },
+                            paginationModel: { page: 0, pageSize: 10 },
                         },
                     }}
-                    pageSizeOptions={[5, 10, 25]}
+                    // pageSizeOptions={[10]}
                     disableRowSelectionOnClick = {true}
                 />
                 <FormModal modalType={"add" + dataType} open={openAdd} handleClose={handleCloseAdd} handleClickOpen={handleClickOpenAdd}/>
