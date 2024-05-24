d4cf21c72027cbb071f7c31dffbcb3af9fbda441
Patrick Yuson <ntu7de@virginia.edu>
2024-05-23 22:14:13 -0400

    Made a working main.jsx that renders the landing page first without the navbar

--- src/routes/LandingPage.jsx
+++ src/routes/LandingPage.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
+import Button from '@mui/material/Button'
+import { useNavigate } from 'react-router-dom'
 
 const LandingPage = () => {
-  return (
-    <>
-        <h1>Hello World!!</h1>
-    </>
-  )
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/dashboard');
+    }
+
+    return (
+        <>
+            <h1>Hey, Patriot!</h1>
+            <Button
+                sx={{
+                    background: '#6246EA',
+                    border: '1px solid rgb(89, 89, 89)',
+                    textTransform: 'none',
+                    boxShadow: 'none',
+                    fontFamily: 'Inter, system-ui, Avenir, Helvetica, Arial, sans-serif',
+                    fontWeight:400,
+                    '&:hover': {
+                    boxShadow: 'none',
+                    backgroundColor: '#fffffe',
+                    color: '#2b2c34'
+                    },
+                }}
+                style={{float: "right"}}
+                className="but"
+                variant="contained"
+                onClick={handleClick}
+            >Let's Get Started!</Button> 
+        </>
+    )
 }
 
 export default LandingPage
