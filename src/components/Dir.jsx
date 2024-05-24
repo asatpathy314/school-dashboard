2515aa82a1638653c51d76dbb1d607ad0181da23
Patrick Yuson <ntu7de@virginia.edu>
2024-05-23 21:34:16 -0400

    Added a 'Home' button for all 'Dir' objects
    The button is placed in the top right of the class 'container' -- it needs to be styled so that it's in the top LEFT instead

--- src/components/Dir.jsx
+++ src/components/Dir.jsx
@@ -2,11 +2,14 @@ import { useState } from 'react';
 import Button from '@mui/material/Button';
 import FormModal from './FormModal';
 import AddEvent from './modals/AddEvent';
+import { useNavigate } from 'react-router-dom';
 import './../styles/dir.css'
 
 const Dir = (data) => {
     const [openAdd, setOpenAdd] = useState(false);
     const [openRemove, setOpenRemove] = useState(false);
+    const navigate = useNavigate();
+
     const handleClickOpenAdd = () => {
         setOpenAdd(true);
     };
@@ -22,9 +25,32 @@ const Dir = (data) => {
         setOpenRemove(false);
     };    
 
+    const handleBack = () => {
+        navigate('/');
+    }
+
     return (
         <>
             <div className='container'>
+                <Button
+                    sx={{
+                        background: '#6246EA',
+                        border: '1px solid rgb(89, 89, 89)',
+                        textTransform: 'none',
+                        boxShadow: 'none',
+                        fontFamily: 'Inter, system-ui, Avenir, Helvetica, Arial, sans-serif',
+                        fontWeight:400,
+                        '&:hover': {
+                        boxShadow: 'none',
+                        backgroundColor: '#fffffe',
+                        color: '#2b2c34'
+                        },
+                    }}
+                    style={{float: "right"}}
+                    className="but"
+                    variant="contained"
+                    onClick={handleBack}
+                >Home</Button> 
                 <h2 className='h2'>{data['type'] === 'indClass' ? data['name'] : data['type'] + ' Directory'}</h2>
                 <div className='overall'>
                     <div className='list'>
